949b9431d5005c33881b2900f0ceabfa05279754
Sahil Malhotra <sahil061626@gmail.com>
2024-08-27 21:43:51 +0530

    intial commit for command handling

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿
 using ExpenseTracker.Utilities;
-    
+
 ConsoleMessage.PrintInfoMessage("Hi, Welcome To Expense Tracker Application : ");
 
 ConsoleMessage.PrintCommandMessage("Please enter command to continue or type help for more options : ");
@@ -16,12 +16,59 @@ while (true)
 
     List<string> commands = Utility.ParseInput(input);
 
+    if(commands.Count < 2)
+    {
+        ConsoleMessage.PrintErrorMessage("Wrong command entered!");
+        continue;
+    }
+
     if (!commands[0].ToLower().Equals("expense-tracker"))
     {
         ConsoleMessage.PrintErrorMessage("Wrong command entered!");
         continue;
     }
+
+    HandleEnteredExpense(commands);
+
 }
 
+void HandleEnteredExpense(List<string> commands)
+{
+    switch (commands[1].ToLower())
+    {
+        case "add":
+            AddExpense();
+            break;
+        case "list":
+            DisplayAddedExpenses();
+            break;
+        case "summary":
+            DisplaySummary();
+            break;
+        case "delete":
+            DeleteExpense();
+            break;
+        default:
+            break;
+    }
+}
 
+void DeleteExpense()
+{
+    throw new NotImplementedException();
+}
 
+void DisplaySummary()
+{
+    throw new NotImplementedException();
+}
+
+void DisplayAddedExpenses()
+{
+    throw new NotImplementedException();
+}
+
+void AddExpense()
+{
+    throw new NotImplementedException();
+}
